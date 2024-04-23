7efc1583c2fad5eb19e39d7d243ae43bfc13f9e5
jenskat88 <jenskat88@gmail.com>
2024-04-23 12:47:25 -0400

    finished work section

---
+++ Assets/JS/script.js
@@ -0,0 +1,19 @@
+/*========= MixItUp Filter ==========*/
+let mixerProjects = mixitup('.project__container', {
+    selectors: {
+        target: '.project__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Active Work */
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork() {
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
+}
+
+linkWork.forEach((a) => a.addEventListener('click', activeWork));
